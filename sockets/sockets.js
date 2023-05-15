700bfd6ccbf35b15c614237dd1b44e2486519fe1
Jorge Perez <jorge.perez@puxles.com>
2023-05-15 13:51:24 -0500

    Implementación CRUD en cache

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,15 +1,46 @@
 
-const { io } = require('../index')
+const { io } = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand( new Band('Aerosmith') );
+bands.addBand( new Band('Musiko') );
+bands.addBand( new Band('Thirday') );
+bands.addBand( new Band('Systema Solar') );
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
     
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
 
-    client.on('mensaje', (payload) => {
+    // Escuchar evento para setear voto
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        
+        // Se envía a todos los clientes, inclyendo al emisor.
+        io.emit('active-bands', bands.getBands());
+    });
 
+    // Escuchar evento para agragar banda
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        
+        // Se envía a todos los clientes, inclyendo al emisor.
+        io.emit('active-bands', bands.getBands());
+    });
+
+    // Escuchar evento para borrar banda
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        
+        // Se envía a todos los clientes, inclyendo al emisor.
+        io.emit('active-bands', bands.getBands());
     });
 });
